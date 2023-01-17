df564ccf39440345b280d0bb60bae9b23ec3f058
Mariana <marianaamaralsteffen@gmail.com>
2023-01-17 06:45:21 -0300

    Acrescentado JavaDoc

--- Funcoes.java
+++ Funcoes.java
@@ -1,33 +1,61 @@
-
 import java.util.ArrayList;
+
+/**
+ * Classe desinada às funções utilizadas na classe Main
+ * @author Mariana Steffen
+ */
 
 public class Funcoes {
 
-    public static boolean checagem(int num, String tipo) {
-        if(tipo.contentEquals("primos")) {
-            if(num < 1) {
-                System.out.println("Valor inválido. Digite novamente.");
-                return false;
-            } else {
-                return true;
-            }
-        } else if(tipo.contentEquals("fibonacci")) {
-            if(num <= 0) {
-                System.out.println("Valor inválido.");
-                return false;
-            } else {
-                return true;
-            }
-        } else {
+    /**
+     * Checagem de input para as funções de Fibonacci
+     * @param num valor a ser avaliado
+     * @return boolean de confirmação de input
+     */
+
+    public static boolean checagemFibonacci(int num) {
+
+        if(num <= 0) {
+            System.out.println("Valor inválido. Digite novamente.");
             return false;
+        } else {
+            return true;
         }
     }
+
+    /**
+     * Checagem de input para as funções de primos
+     * @param num valor a ser avaliado
+     * @return boolean de confirmação de input
+     */
+
+    public static boolean checagemPrimos(int num) {
+
+        if(num < 1) {
+            System.out.println("Valor inválido. Digite novamente.");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Função recursiva para a obtenção de um número na sequência Fibonacci
+     * @param num número da sequência Fibonacci a ser retornado
+     * @return int correspondente ao parametro num recebido na sequencia Fibonacci
+     */
 
     public static int fibonacciRecursivo(int num) {
         if(num <= 0) {return 0;}
         else if (num == 1) {return 1;}
         else {return  fibonacciRecursivo(num-1) + fibonacciRecursivo(num-2);}
     }
+
+    /**
+     * Função linear para a obtenção de um número na sequência Fibonacci.
+     * @param num número da sequência Fibonacci a ser retornado
+     * @return int correspondente ao parametro num recebido na sequencia Fibonacci
+     */
 
     public static int fibonacciLinear(int num) {
         int temp1 = 0;
@@ -45,6 +73,13 @@ public class Funcoes {
             return resultado;
         }
     }
+
+    /**
+     * Função recursiva para a obtenção de uma lista de números primos até determinado valor
+     * @param num valor mais alto que a sequência de primos pode ter
+     * @param lista lista dos números primos encontrados
+     * @return lista dos números primos encontrados
+     */
 
     public static ArrayList<Integer> primosRecursivo(int num, ArrayList<Integer> lista) {
         int primo = 0;
@@ -64,6 +99,13 @@ public class Funcoes {
             return primosRecursivo(num - 1, lista);
         }
     }
+
+    /**
+     * Função linear para a obtenção de uma lista de números primos até determinado valor
+     * @param num valor mais alto que a sequência de primos pode ter
+     * @param lista lista dos números primos encontrados
+     * @return lista dos números primos encontrados
+     */
 
     public static ArrayList<Integer> primosLinear(int num, ArrayList<Integer> lista) {
         int primo = 0;
--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Classe principal para testes
+ * @author Mariana Steffen
+ */
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -8,23 +13,23 @@ public class Main {
         boolean valInput;
 
         // Teste fibonacci linear
-        System.out.println("Digite um número: ");
+        System.out.println("Digite um número (fibonacci): ");
 
         valInput = false;
         while(!valInput) {
             num = scanner.nextInt();
-            valInput = Funcoes.checagem(num, "fibonacci");
+            valInput = Funcoes.checagemFibonacci(num);
         }
 
         System.out.println(Funcoes.fibonacciLinear(num));
 
         // Teste fibonacci recursivo
-        System.out.println("Digite um número: ");
+        System.out.println("Digite um número (fibonacci): ");
 
         valInput = false;
         while(!valInput) {
             num = scanner.nextInt();
-            valInput = Funcoes.checagem(num, "fibonacci");
+            valInput = Funcoes.checagemFibonacci(num);
         }
 
         System.out.println(Funcoes.fibonacciRecursivo(num));
@@ -32,24 +37,24 @@ public class Main {
         ArrayList<Integer> primos = new ArrayList<>();
 
         // Teste primos linear
-        System.out.println("Digite um número: ");
+        System.out.println("Digite um número (primos): ");
 
         valInput = false;
         while(!valInput) {
             num = scanner.nextInt();
-            valInput = Funcoes.checagem(num, "primos");
+            valInput = Funcoes.checagemPrimos(num);
         }
 
         System.out.println(Funcoes.primosLinear(num, primos));
         primos.clear();
 
         // Teste primos recursivo
-        System.out.println("Digite um número: ");
+        System.out.println("Digite um número (primos): ");
 
         valInput = false;
         while(!valInput) {
             num = scanner.nextInt();
-            valInput = Funcoes.checagem(num, "primos");
+            valInput = Funcoes.checagemPrimos(num);
         }
 
         System.out.println(Funcoes.primosRecursivo(num, primos));
